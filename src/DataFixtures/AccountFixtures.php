293ccf201923dbf335bc027482b9a5ebf98a4421
Moritz Mögel <moegelmoritz@gmail.com>
2023-03-02 14:41:05 +0100

    modified:   composer.json 	modified:   composer.lock 	modified:   config/bundles.php 	new file:   src/DataFixtures/AccountFixtures.php 	new file:   src/DataFixtures/UserFixtures.php 	modified:   symfony.lock

---
+++ src/DataFixtures/AccountFixtures.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Account;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class AccountFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $account = new Account();
+        $account->setAccountHolderName('superadmin');
+        $account->setAccountNumber(420);
+        $account->setAccountType('DNP');
+        $account->setStatusOfAccount(1);
+        $account->setIban('wet8ug4iugu4gh4');
+        $account->setBic('cock');
+        $account->setUser($this->getReference('superadmin'));
+        $manager->persist($account);
+
+        $account = new Account();
+        $account->setAccountHolderName('kalle');
+        $account->setAccountNumber(420);
+        $account->setAccountType('DNP');
+        $account->setStatusOfAccount(1);
+        $account->setIban('wet8ug4iugu4gh4');
+        $account->setBic('cock');
+        $account->setUser($this->getReference('kalle'));
+        $manager->persist($account);
+
+        $account = new Account();
+        $account->setAccountHolderName('marianne');
+        $account->setAccountNumber(420);
+        $account->setAccountType('DNP');
+        $account->setStatusOfAccount(1);
+        $account->setIban('wet8ug4iugu4gh4w');
+        $account->setBic('cockc');
+        $account->setUser($this->getReference('marianne'));
+        $manager->persist($account);
+
+        $account = new Account();
+        $account->setAccountHolderName('zork');
+        $account->setAccountNumber(420);
+        $account->setAccountType('DNP');
+        $account->setStatusOfAccount(1);
+        $account->setIban('wet8ug4iugu4gh344');
+        $account->setBic('coc121k');
+        $account->setUser($this->getReference('zork'));
+        $manager->persist($account);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class,
+        ];
+    }
+
+}
